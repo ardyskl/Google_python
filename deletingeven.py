bdc90a88de24a4118b67c3ab27f02519af9407dc
AaradhyaShukla <ayn.j.rand@gmail.com>
2021-07-12 03:00:47 +0800

    Added a print statement in the start and end

--- deletingeven.py
+++ deletingeven.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-import random
 
-random_lst = []
-for i in range(0, 50):
-    n = random.randint(1, 30)
-    random_lst.append(n)
-return random_lst
+print("We'll test deleting even numbers from starting of a list")
 
-#def delete_starting_even(random_lst):
-#    while (len(random_lst) > 0 and random_lst[0] % 2 == 0):
-#        random_lst = random_lst[1:]
-#    return lst
+def delete_starting_even(lst):
+    while (len(lst) > 0 and lst[0] % 2 == 0):
+       lst = lst[1:]
+    return lst
+
 # Testing with random list elements
-#print(delete_starting_even(random_lst)
-#print(delete_starting_even(random_lst)
+print("New list: ")
+print(delete_starting_even([4, 8, 10, 11, 12, 15]))
+print("New list: ")
+print(delete_starting_even([4, 8, 10, 11, 17,]))
