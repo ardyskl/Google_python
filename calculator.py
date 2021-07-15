56041b7f74418facae0a7b1c72e31b18a50a298b
AaradhyaShukla <ayn.j.rand@gmail.com>
2021-07-15 22:41:48 +0800

    Added a function to handle KeyboardInterruption error, if user presses ^C, clean exit

--- calculator.py
+++ calculator.py
@@ -1,48 +1,61 @@
 #!/usr/bin/env python3
 
 import math
+import sys
+import traceback
 
-# A simple calculator
-def add (a, b):
-    return a + b
+def main():
+    try:
+        # a simple calculator
+        def add (a, b):
+            return a + b
 
-def subtract (a, b):
-    return a - b
+        def subtract (a, b):
+            return a - b
 
-def multiply(a, b):
-    return a * b
+        def multiply(a, b):
+            return a * b
 
-def divide(a, b):
-    return a / b
+        def divide(a, b):
+            return a / b
 
-print("Welcome to our simple calculator!")
+        print("Welcome to our simple calculator!")
 
-cont = "y"
-while(cont.lower() == "y"):
+        cont = "y"
+        while(cont.lower() == "y"):
 
-    choice = input("Please choose an operation: (+, -, *, /): ")
+            choice = input("Please choose an operation: (+, -, *, /): ")
 
-    if choice in ('+', '-', '*', '/'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+            if choice in ('+', '-', '*', '/'):
 
-        if choice == '+':
-            print(num1, "+", num2, "= {0}".format(add(num1, num2)))
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
 
-        elif choice == '-':
-            print(num1, "+", num2, "= {0}".format(subtract(num1, num2)))
+                if choice == '+':
+                    print(num1, "+", num2, "= {0}".format(add(num1, num2)))
 
-        elif choice == '*':
-            print(num1, "*", num2, "= {0}".format(multiply(num1, num2)))
+                elif choice == '-':
+                    print(num1, "-", num2, "= {0}".format(subtract(num1, num2)))
 
-        elif choice == '/':
-            print(num1, "+", num2, "= {0}".format(divide(num1, num2)))
+                elif choice == '*':
+                    print(num1, "*", num2, "= {0}".format(multiply(num1, num2)))
 
-    else:
-        print("Please enter a valid operation: Try again")
+                elif choice == '/':
+                    print(num1, "/", num2, "= {0}".format(divide(num1, num2)))
 
-    cont = input("Do you want another calculation? [y to continue] & [n to exit]: ")
+            else:
+                print("Please enter a valid operation: Try again")
 
-    if cont == "n":
-        print("Thanks for using our calculator app!")
-        quit()
+            cont = input("Do you want another calculation? [y to continue] & [n to exit]: ")
+
+            if cont == "n":
+                print("Thanks for using our calculator app!")
+
+    except KeyboardInterrupt:
+        print("\n--- Shutdown requested exiting ---")
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
