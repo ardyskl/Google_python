da0621ddcab7ca25a8ba68a88210f4a8f564ed54
AaradhyaShukla <ayn.j.rand@gmail.com>
2021-07-16 16:37:55 +0800

    Testing sort function

---
+++ sort.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import math
+
+list_of_names = ["Parker", "Drew", "Cameron", "Logan", "Alex", "Chris", "Terry", "Jamie", "Jordan", "Taylor"]
+list_of_names.sort()
+
+print(list_of_names)
