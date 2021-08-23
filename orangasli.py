e5591066a5e67c0c329cdda3b0f483461e16ab3e
Ardy <ayn.j.rand@gmail.com>
2021-08-23 16:44:39 +0800

    A simple choice script to help Riya

---
+++ orangasli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("Orangalsi topics for final project")
+print("A series of 12 photos\n")
+
+print("Choices:")
+choice = ['culture', 'traditions', 'living', 'lifestyle', 'poverty', 'issues' 'challenges', 'clothing', 'entertainment', 'food', 'education']
+for element in choice:
+    print(element)
