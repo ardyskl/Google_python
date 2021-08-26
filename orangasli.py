8298d5461fb56677c7081297e309e63e0433e5d6
Ardy <ayn.j.rand@gmail.com>
2021-08-26 14:51:33 +0800

    choices are now numbered

--- orangasli.py
+++ orangasli.py
@@ -4,6 +4,6 @@ print("Orangalsi topics for final project")
 print("A series of 12 photos\n")
 
 print("Choices:")
-choice = ['culture', 'traditions', 'living', 'lifestyle', 'poverty', 'issues' 'challenges', 'clothing', 'entertainment', 'food', 'education']
-for element in choice:
-    print(element)
+choice = ['culture', 'traditions', 'living', 'lifestyle', 'poverty', 'issues', 'challenges', 'clothing', 'entertainment', 'food', 'education', 'indigenous knowledge', 'health']
+for number, word in enumerate(choice, 1):
+    print(number, word)
